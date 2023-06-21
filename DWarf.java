410a175efe59066b92dcbb1f9c9102aab5dca99a
7_sav.age_7 <109322708+MirosChavi@users.noreply.github.com>
2023-06-22 00:43:44 +0200

    Add files via upload

---
+++ DWarf.java
@@ -0,0 +1,23 @@
+package rpg;
+
+public class Dwarf extends Hero{
+    private static String[] imiona = {"Ogr", "Minion", "Gerald", "Milena"};
+    public Dwarf(String name, int health, int strength) {
+        super(name, health, strength);
+    }
+    public static Dwarf getInstance(){
+        String imie = imiona[rand.nextInt(imiona.length)];
+        int zdrowie = rand.nextInt(50,100);
+        int sila = rand.nextInt(20,40);
+        return new Dwarf(imie, zdrowie, sila);
+    }
+
+    @Override
+    public void attack(Hero another) {
+        int damage = (int)(this.strength * rand.nextDouble(0.5,1));
+        //int damage = (int)(this.strength * (rand.nextDouble()*0.5 +0.5));
+        another.health -= damage;
+        System.out.println(this.name + " attacks " + another.name +
+                " and makes " +damage+ " points of damage.");
+    }
+}
