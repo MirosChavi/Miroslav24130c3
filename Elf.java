0b0a861c6e6232e5ed87a052b37aa5ac2581d0b5
7_sav.age_7 <109322708+MirosChavi@users.noreply.github.com>
2023-06-22 00:51:02 +0200

    Elf.java

--- Elf.java
+++ Elf.java
@@ -1,7 +1,7 @@
 package rpg;
 
 public class Elf extends Hero{
-    private static String[] imiona = {"Erro", "Legolas", "Alfsol", "Kaldia"};
+    private static String[] imiona = {"Erro", "Legolas", "Alfsol", "Kaldia"}; // Nadajemy imiona
     public Elf(String name, int health, int strength) {
         super(name, health, strength);
     }
