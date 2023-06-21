ee1222794fcb8c30afd0a921dd290247710e7fc5
7_sav.age_7 <109322708+MirosChavi@users.noreply.github.com>
2023-06-22 00:55:39 +0200

    Hero.java

--- Hero.java
+++ Hero.java
@@ -2,39 +2,39 @@ package rpg;
 
 import java.util.Random;
 
-public abstract class Hero {
+public abstract class Hero { // Tworzymy zmienne
     protected String name;
     protected int health;
     protected int strength;
     protected static Random rand = new Random();
 
-    public Hero(String name, int health, int strength) {
+    public Hero(String name, int health, int strength) { // używamy konstructorza
         this.name = name;
         this.health = health;
         this.strength = strength;
     }
 
-    public String getName() {
+    public String getName() { // Przy pomocy alt inów szybko generujemy taki kod za pomocą getterów i setterów
         return name;
     }
 
-    public void setName(String name) {
+    public void setName(String name) { // Przy pomocy alt inów szybko generujemy taki kod za pomocą getterów i setterów
         this.name = name;
     }
 
-    public int getHealth() {
+    public int getHealth() { // Przy pomocy alt inów szybko generujemy taki kod za pomocą getterów i setterów
         return health;
     }
 
-    public void setHealth(int health) {
+    public void setHealth(int health) { // Przy pomocy alt inów szybko generujemy taki kod za pomocą getterów i setterów
         this.health = health;
     }
 
-    public int getStrength() {
+    public int getStrength() { // Przy pomocy alt inów szybko generujemy taki kod za pomocą getterów i setterów
         return strength;
     }
 
-    public void setStrength(int strength) {
+    public void setStrength(int strength) { // Przy pomocy alt inów szybko generujemy taki kod za pomocą getterów i setterów
         this.strength = strength;
     }
 
